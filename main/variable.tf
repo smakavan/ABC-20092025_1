8f3b5fe8a45c2192229e06227c60350cba95681a
smakavan <sachinmakavana@gmail.com>
2025-09-21 12:29:26 +0530

    Create variable.tf

---
+++ main/variable.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "admin_username" {
+    type = string
+}
+
+variable "admin_ssh_key" {
+    type = string
+}
