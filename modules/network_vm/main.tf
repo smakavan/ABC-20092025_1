b0ae753d5f94439135422fa8e17546d9f9a1a408
smakavan <sachinmakavana@gmail.com>
2025-09-21 12:41:53 +0530

    Create main.tf

---
+++ modules/network_vm/main.tf
@@ -0,0 +1,164 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_virtual_network" "example-1" {
+  name                = "peternetwork1"
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.5.0.0/16"]
+  location            = azurerm_resource_group.example.location
+}
+
+resource "azurerm_virtual_network" "example-2" {
+  name                = "peternetwork2"
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.15.0.0/16"]
+  location            = azurerm_resource_group.example.location
+}
+
+resource "azurerm_virtual_network_peering" "example-1" {
+  name                      = "peer1to2"
+  resource_group_name       = azurerm_resource_group.example.name
+  virtual_network_name      = azurerm_virtual_network.example-1.name
+  remote_virtual_network_id = azurerm_virtual_network.example-2.id
+}
+
+resource "azurerm_virtual_network_peering" "example-2" {
+  name                      = "peer2to1"
+  resource_group_name       = azurerm_resource_group.example.name
+  virtual_network_name      = azurerm_virtual_network.example-2.name
+  remote_virtual_network_id = azurerm_virtual_network.example-1.id
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example-1.name
+  address_prefixes     = ["10.5.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example-2.name
+  address_prefixes     = ["10.15.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic_vm1" {
+  name                = "nic_vm1"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm1_public_ip.id
+  }
+}
+
+resource "azurerm_public_ip" "vm1_public_ip" {
+  name                = "vm1-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku = "Standard"
+}
+
+resource "azurerm_network_interface" "nic_vm2" {
+  name                = "nic-vm2"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet2.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_vm1_nsg" {
+  network_interface_id      = azurerm_network_interface.nic_vm1.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_vm2_nsg" {
+  network_interface_id      = azurerm_network_interface.nic_vm2.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "VM1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.nic_vm1.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.admin_ssh_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                = "VM2"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.nic_vm2.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.admin_ssh_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
