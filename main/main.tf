bedcbb38639d89c710cea2c3595c6e6e99ccfeec
smakavan <sachinmakavana@gmail.com>
2025-09-21 12:28:18 +0530

    Create main.tf

---
+++ main/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+     azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = "a0c353b6-cb40-4e36-a692-5ce1da78943d"
+}
+
+module "network_vm" {
+  source = "./module"
+
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  admin_username      = var.admin_username
+  admin_ssh_key       = var.admin_ssh_key
+}
